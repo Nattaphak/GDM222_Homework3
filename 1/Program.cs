ec93b7b787caa48397be7385bc79e9164394df82
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2023-03-28 20:44:35 +0700

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        double xP1 = double.Parse(Console.ReadLine());
+        double yP1 = double.Parse(Console.ReadLine());
+
+        double xP2 = double.Parse(Console.ReadLine());
+        double yP2 = double.Parse(Console.ReadLine());
+
+        double xP3 = double.Parse(Console.ReadLine());
+        double yP3 = double.Parse(Console.ReadLine());
+
+        double h = ((yP2-yP1) * (yP3*yP3-yP1*yP1+xP3*xP3-xP1*xP1) + (yP3-yP1) * (yP1*yP1-yP2*yP2+xP1*xP1-xP2*xP2)) / (2 * (xP3-xP1) * (yP2-yP1) -2 * (xP2-xP1) * (yP3-yP1));
+        double k = ((xP2-xP1) * (xP3*xP3-xP1*xP1+yP3*yP3-yP1*yP1) + (xP3-xP1) * (xP1*xP1-xP2*xP2+yP1*yP1-yP2*yP2)) / (2 * (yP3-yP1) * (xP2-xP1) -2 * (yP2-yP1) * (xP3-xP1));
+
+        double r =Math.Sqrt((xP1-h) * (xP1-h) + (yP1-k) * (yP1-k));
+
+        Console.WriteLine(h);
+        Console.WriteLine(k);
+        Console.WriteLine(r);
+        
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,98 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Vector2> point = new List<Vector2>();
+        bool CheckInside = true;
+
+        for(int i = 0; i <= 0;)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            if(x != 0 || y != 0)
+            {
+                point.Add(new Vector2(x ,y));
+            }
+
+            else
+            {
+                i++;
+            }
+            
+        }
+
+        double xK = double.Parse(Console.ReadLine());
+        double yK = double.Parse(Console.ReadLine());
+
+        int n = 0;
+        while( n < point.Count() && CheckInside == true)
+        {
+            if(n != point.Count()-1)
+            {
+                double slope = (point[point.Count()-1].y - point[0].y)/(point[point.Count()-1].x - point[0].x);
+                if(slope < 0)
+                {
+                    if(((1 / slope) * yK) - (xK) <= (1 / slope) * point[n].y - (point[n].x)) 
+                    {
+                        CheckInside = true;
+                    }
+                    else if(((1 / slope) * yK) - (xK) > (1 / slope) * point[n].y - (point[n].x))
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if(((1 / slope) * yK) - (xK) <= (1 / slope) * point[n].y - (point[n].x))  
+                    {
+                        CheckInside = true;
+                    }
+                    else if(((1 / slope) * yK) - (xK) > (1 / slope) * point[n].y - (point[n].x))
+                    {
+                        CheckInside = false;
+                        break;
+                    }
+                }   
+            }
+
+             else if(n == point.Count()-1)
+            {
+                double slope = (point[point.Count()-1].y - point[0].y)/(point[point.Count()-1].x - point[0].x);
+
+                 if(slope < 0)
+                {
+                    if(((1 / slope) * yK) - (xK) <= (1 / slope) * point[n].y - (point[n].x)) 
+                    {
+                        CheckInside = true;
+                    }
+                    else if(((1 / slope) * yK) - (xK) > (1 / slope) * point[n].y - (point[n].x))
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    if(((1 / slope) * yK) - (xK) <= (1 / slope) * point[n].y - (point[n].x))  
+                    {
+                        CheckInside = true;
+                    }
+                    else if(((1 / slope) * yK) - (xK) > (1 / slope) * point[n].y - (point[n].x))
+                    {
+                        CheckInside = false;
+                        break;
+                    }
+                } 
+            }
+            n++;
+        }
+         if(CheckInside == true) 
+        {
+            Console.WriteLine("Inside");
+        }
+        else if(CheckInside == false)
+        {
+            Console.WriteLine("Outside");
+        }
+    }
+}
